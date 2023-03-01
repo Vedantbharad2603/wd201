5f00056d5d369569b153acfa40bd5c04d64e25e7
Vedantbharad2603 <vedantbharad26@gmail.com>
2023-03-01 19:54:19 +0530

    HTTP server

---
+++ http-server/index.js
@@ -0,0 +1,63 @@
+// const http=require("http");
+// const fs=require("fs");
+
+// const server=http.createServer((req,res)=>{
+//     const stream=fs.createReadStream("sample.txt");
+//     stream.pipe(res);
+//     // fs.readFile("sample.txt",(err,data)=>{
+//     //     res.end(data);
+//     // })
+// })
+// server.listen(3000);
+
+const http = require("http");
+const fs = require("fs");
+
+const args = require("minimist")(process.argv.slice(1));
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
+});
+
+fs.readFile("registration.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = project;
+});
+
+http
+    .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            case "/registration":
+                response.write(registrationContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
+    })
+    .listen(3000);
+    // args.port
